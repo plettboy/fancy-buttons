7583b3b5b1e22cccd0d290f62a8c868c25376a30
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-10-03 19:25:06 -0700

    Add files via upload

---
+++ src/components/CounterButton.js
@@ -0,0 +1,19 @@
+import React from 'react'; //optional
+import { useState } from "react";
+
+
+function CounterButton(props){
+  const [clickAmount, setClickAmount] = useState(0);
+  
+  const handleClick = () => {
+    setClickAmount(clickAmount + 1)
+  }
+  
+  return (
+    <button className="CounterButton" onClick={handleClick}>
+    You clicked me {clickAmount} times
+  </button>
+  );
+}
+
+export default CounterButton;
